4adcf4afd4ef50fd45642ed840e016976fc55191
dannyajc <danny1_janssen@msn.com>
2021-12-27 17:09:46 +0100

    Add webshop webview

--- lib/screens/webshop/webshop_screen.dart
+++ lib/screens/webshop/webshop_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:nailstudy_app_flutter/constants.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebshopScreen extends StatelessWidget {
   const WebshopScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: WebView(
+        initialUrl: 'https://www.gailsnailacademy.nl/webshop/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
   }
 }
